e92a66886af2797f3864b9694671c9843221b4e5
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-13 17:57:42 +0530

    right way to connect to db and listen

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,54 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/database');
 
 const app = express();
 
-// app.use("/getUserData", (req, res, next) => {
-//     throw new Error("error in getUserData!");
-//     res.send("user data has been sent!");
-// });
-
-app.use("/getUserData", (req, res) => {
-    try {
-        throw new Error("error while fetching data from db!");
-        res.send("data has been sent!");
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-})
-
-app.use("/", (err, req, res, next) => {
-    if (err) {
-        console.log(err);
-        res.status(500).send("something went wrong!");
-    }
-});
-
-// app.use((req, res, next) => {
-//     res.send("request has been recieved.");
-//     next();
-// });
-
-// app.use("/hello", (req, res) => {
-//     res.send("from hello");
-// })
-
-// app.use("/hello/2", (req, res) => {
-//     res.send("from hello/2");
-// });
+connectDB()
+    .then(() => {
+        console.log("Database connection has been established!");
+        app.listen(3000, () => {
+            console.log("Server is listening on port 3000");
+        });
+    })
+    .catch(() => {
+        console.error("Database connection failed!");
+    })
 
 
-// app.use("/", (req, res) => {
-//     res.send("from /");
-// })
-
-// app.use('/users', (req, res, next) => {
-//     console.log("1st route handler");
-//     next();
-// }, (req, res, next) => {
-//     console.log("from the 2nd route handler");
-//     res.send("respond from 2nd route handler");
-// })
-
-app.listen(3000, () => {
-    console.log("express server is running on port 3000");
-});
+// note : Right process is first connect to the database then listen on port.
