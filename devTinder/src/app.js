abe8aaeeb77096dd4bec386c9adafac75cc598d1
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-13 16:59:17 +0530

    error handling in nodejs

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -2,9 +2,42 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("hello from the dev tinder platform!");
-})
+app.use("/getUserData", (req, res, next) => {
+    throw new Error("error in getUserData!");
+});
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        console.log(err);
+        res.status(500).send("something went wrong!");
+    }
+});
+
+// app.use((req, res, next) => {
+//     res.send("request has been recieved.");
+//     next();
+// });
+
+// app.use("/hello", (req, res) => {
+//     res.send("from hello");
+// })
+
+// app.use("/hello/2", (req, res) => {
+//     res.send("from hello/2");
+// });
+
+
+// app.use("/", (req, res) => {
+//     res.send("from /");
+// })
+
+// app.use('/users', (req, res, next) => {
+//     console.log("1st route handler");
+//     next();
+// }, (req, res, next) => {
+//     console.log("from the 2nd route handler");
+//     res.send("respond from 2nd route handler");
+// })
 
 app.listen(3000, () => {
     console.log("express server is running on port 3000");
