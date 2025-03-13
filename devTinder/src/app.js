b1d44850e11ac5368f0cc6844818bd3ba9ad29b6
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-13 18:30:50 +0530

    save data to database

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const User = require('./models/user');
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+    const userObject = {
+        firstName: "priyanshu",
+        lastName: "singh",
+        emailId: "priyanshuparashar08@gmail.com",
+        gender: "male"
+    };
+    // creating a new instance of user model
+    const user = new User(userObject);
+
+    await user.save();
+    res.send(`data save successfully \n ${user}`)
+});
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("something went wrong!");
+    }
+})
 
 connectDB()
     .then(() => {
