5efba0395876c9a2ab0311d5fa06a4079eb99999
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-15 13:47:02 +0530

    rename and delete some folder

--- devTinder/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://demo:demoPass@democluster.gbs2x.mongodb.net/devTinder"
-    );
-}
-
-module.exports = connectDB;
