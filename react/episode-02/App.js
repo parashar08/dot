c3316e2d71304599995dd57ecb68de4d085ef504
priyanshuBS <priyanshuparashar08@gmail.com>
2025-04-02 23:52:23 +0530

    clear dot

--- react/episode-02/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const headingElement = React.createElement("h1", { id: "heading" }, "zylo");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(headingElement);
